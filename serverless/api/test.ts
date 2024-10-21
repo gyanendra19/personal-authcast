232a554139344287aca47361e8ea4038f648f78e
Rana Paul <ranapaul741223@gmail.com>
2024-10-21 10:51:38 +0530

    update serverless function

--- serverless/api/test.ts
+++ serverless/api/test.ts
@@ -1,6 +1,8 @@
 export async function POST(request: Request) {
   
   const body = await request.json();
+  console.log("body", body);
+  
 
-  Response.json({body})
+  return new Response(`Hello from `)
 }
